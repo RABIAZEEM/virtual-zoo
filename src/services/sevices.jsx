bd6f1d2b27ff946e0fdc5ca62804f67754d73557
@gilsusana <gilsusanaCIBAF5@gmail.com>
2022-09-07 09:57:09 +0200

    service-axios

--- src/services/sevices.jsx
+++ src/services/sevices.jsx
@@ -7,7 +7,7 @@ function serviceAxios() {
 export default serviceAxios
 import axios from "axios";
 export const serviceAxios=() => {
-    const url= " http://localhost:5000/animals"
+    const url= "https://zoo-animal-api.herokuapp.com"
     const get =async() => {
          const res = await axios.get(url);
          return res;
@@ -16,20 +16,8 @@ export const serviceAxios=() => {
         const res = await axios.post(url,data);
         return res;
     };
-    const destroy = async (id) => {
-        let urlID=`${url}/${id}`;
-        const res = await axios.delete(urlID)
-        return res;
-    };
-    const update = async (data) => {
-        let urlID= `${url}/${data.id}`;
-        const res = await axios.post(urlID, data);
-        return res;
-    };
+  
     return {
         get,
-        create,
-        destroy,
-        update,
     }
 }
